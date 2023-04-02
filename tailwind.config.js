aba4bf8d4c01914efb8dead6fedf37a794dc03c5
monkonius <65208909+monkonius@users.noreply.github.com>
2023-04-02 12:54:24 +0800

    Change default font to Poppins and change gap

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./docs/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
